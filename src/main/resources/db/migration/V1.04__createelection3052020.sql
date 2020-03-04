6268c2fd403abf97248b4997b69d8f362c9fbba5
Charles Amper <amperca@ph.ibm.com>
2020-03-05 04:34:31 +0800

    rename dll and update entities

--- src/main/resources/db/migration/V1.04__createelection3052020.sql
+++ src/main/resources/db/migration/V1.04__createelection3052020.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `election` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
+  `status` int(11) DEFAULT 0,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
