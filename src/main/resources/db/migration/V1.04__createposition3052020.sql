db2b6c3471370d93b6f7942ec0ddc41513a425a0
Charles Amper <amperca@ph.ibm.com>
2020-03-05 03:46:35 +0800

    add election dll

--- src/main/resources/db/migration/V1.04__createposition3052020.sql
+++ src/main/resources/db/migration/V1.04__createposition3052020.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `position` (
+CREATE TABLE `election` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unq_position_position_name` (`name`)
+  UNIQUE KEY `unq_election_election_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
