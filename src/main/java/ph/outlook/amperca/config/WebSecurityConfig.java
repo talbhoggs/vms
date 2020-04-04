f8e72f9fc28073bd05826a8bf628415a56d739c8
Charles Amper <amperca@ph.ibm.com>
2020-04-04 23:51:07 +0800

    progress on security

--- src/main/java/ph/outlook/amperca/config/WebSecurityConfig.java
+++ src/main/java/ph/outlook/amperca/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package ph.outlook.amperca.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,32 +8,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-   
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
         http
             .authorizeRequests()
-            .antMatchers("/api/**")
+            .antMatchers("/")
             .permitAll()
             .anyRequest()
             .authenticated()
-            .and().formLogin().loginPage("/login");
-    }
-    
-    @Override
-    protected UserDetailsService userDetailsService() {
-        // TODO Auto-generated method stub
-        
-        UserDetails amperca = User.builder().username("amperca").password("passord").roles("VOTER").build();
-        
-        
-        return new InMemoryUserDetailsManager(amperca);
+            //.and().formLogin().loginPage("/login");
+            .and().httpBasic();
     }
 
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        // TODO Auto-generated method stub
+        UserDetails amperca = User.builder().
+                            username("amperca").
+                            password(passwordEncouEncoder().encode("password")).
+                            roles("VOTER").build();
+        UserDetails admin = User.builder().
+                                username("admin").
+                                password(passwordEncouEncoder().encode("password")).
+                                roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(amperca, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncouEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 }
