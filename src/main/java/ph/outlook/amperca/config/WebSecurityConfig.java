fffbbdc38a550e8585eb9343db563af07aa3be36
Charles Amper <amperca@ph.ibm.com>
2020-04-06 23:54:43 +0800

    progress on security

--- src/main/java/ph/outlook/amperca/config/WebSecurityConfig.java
+++ src/main/java/ph/outlook/amperca/config/WebSecurityConfig.java
@@ -18,10 +18,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
             .authorizeRequests()
-            .antMatchers("/")
-            .permitAll()
+            .antMatchers("/api/**").hasRole("ADMIN")
             .anyRequest()
             .authenticated()
             //.and().formLogin().loginPage("/login");
